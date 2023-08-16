a2869f395661360c1b1223410802073f1d3e6f23
RIG <server@aymar.tech>
2023-08-16 14:18:38 +0300

    add test

--- PyTorch/SpeechSynthesis/FastPitch/hifigan/models.py
+++ PyTorch/SpeechSynthesis/FastPitch/hifigan/models.py
@@ -235,20 +235,21 @@ class Denoiser(nn.Module):
     """ Removes model bias from audio produced with hifigan """
 
     def __init__(self, hifigan, filter_length=1024, n_overlap=4,
-                 win_length=1024, mode='zeros', **infer_kw):
+                 win_length=1024, mode='zeros', device='cuda', **infer_kw):
         super().__init__()
+        self.device = torch.device(device)
         self.stft = STFT(filter_length=filter_length,
                          hop_length=int(filter_length/n_overlap),
-                         win_length=win_length).cuda()
+                         win_length=win_length).to(self.device)
 
         for name, p in hifigan.named_parameters():
             if name.endswith('.weight'):
-                dtype = p.dtype
-                device = p.device
+                model_dtype = p.dtype
+                model_device = p.device
                 break
 
         mel_init = {'zeros': torch.zeros, 'normal': torch.randn}[mode]
-        mel_input = mel_init((1, 80, 88), dtype=dtype, device=device)
+        mel_input = mel_init((1, 80, 88), dtype=model_dtype, device=model_device)
 
         with torch.no_grad():
             bias_audio = hifigan(mel_input, **infer_kw).float()
@@ -264,7 +265,7 @@ class Denoiser(nn.Module):
         self.register_buffer('bias_spec', bias_spec[:, :, 0][:, :, None])
 
     def forward(self, audio, strength=0.1):
-        audio_spec, audio_angles = self.stft.transform(audio.cuda().float())
+        audio_spec, audio_angles = self.stft.transform(audio.to(self.device).float())
         audio_spec_denoised = audio_spec - self.bias_spec * strength
         audio_spec_denoised = torch.clamp(audio_spec_denoised, 0.0)
         audio_denoised = self.stft.inverse(audio_spec_denoised, audio_angles)
