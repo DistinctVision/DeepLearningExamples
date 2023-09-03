5df66e1d46ccd6cd43b13c32a2099b2d23f0c2d3
RIG <server@aymar.tech>
2023-09-03 19:39:46 +0300

    update preparation stripts

--- PyTorch/SpeechSynthesis/FastPitch/scripts/prepare_dataset.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/prepare_dataset.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-: ${DATA_DIR:=LJSpeech-1.1}
+: ${DATA_DIR:=/home/server2/datasets/hi_fi_tts_v0}
+: ${LIST_FILE:=filelists/Cori_Samuel_audio_text.txt filelists/John_Van_Stan_audio_text.txt filelists/Phil_Benson_audio_text.txt}
 : ${ARGS="--extract-mels"}
 
-python prepare_dataset.py \
-    --wav-text-filelists filelists/ljs_audio_text.txt \
+python3 prepare_dataset.py \
+    --wav-text-filelists $LIST_FILE \
     --n-workers 16 \
     --batch-size 1 \
     --dataset-path $DATA_DIR \
     --extract-pitch \
     --f0-method pyin \
+    --n-speakers 4 \
     $ARGS
